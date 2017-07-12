e5815935fcedf5b26c41ac17c79e2125d45b9dbf
phDooY <d.semenduev@gmail.com>
2017-07-12 16:29:25 +0200

    i just can not stand it anymore

--- hw1/deco.py
+++ hw1/deco.py
@@ -4,15 +4,17 @@
 from functools import update_wrapper, wraps
 
 
-def disable():
+def disable(func):
     '''
     Disable a decorator by re-assigning the decorator's name
-    to this function. For example, to turn off memoization:
+    to this function. For example, to turn off memorization:
 
     >>> memo = disable
 
     '''
-    return
+    def wrapper(*args):
+        func.__name__ = disable
+    return wrapper
 
 
 def decorator(func):
@@ -20,28 +22,36 @@ def decorator(func):
     Decorate a decorator so that it inherits the docstrings
     and stuff from the function it's decorating.
     '''
-    @wraps(func)
     def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
+        func(*args, **kwargs)
     return wrapper
 
 
 def countcalls(func):
     '''Decorator that counts calls made to the function decorated.'''
-    def wrapper(*args):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
         wrapper.calls += 1
-        return func(*args)
+        return func(*args, **kwargs)
     wrapper.calls = 0
     wrapper.__name__ = func.__name__
     return wrapper
 
 
-def memo():
+def memo(func):
     '''
     Memoize a function so that it caches all return values for
     faster future lookups.
     '''
-    return
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if wrapper.cache.get(args):
+            return wrapper.cache.get(args)
+        else:
+            wrapper.cache[args] = func(*args, **kwargs)
+            return func(*args, **kwargs)
+    wrapper.cache = {}
+    return wrapper
 
 
 def n_ary(func):
@@ -61,8 +71,8 @@ def n_ary(func):
     return wrapper
 
 
-def trace():
-    '''Trace calls made to function decorated.
+def trace(prefix):
+    """Trace calls made to function decorated.
 
     @trace("____")
     def fib(n):
@@ -80,11 +90,15 @@ def trace():
     ____ <-- fib(1) == 1
      <-- fib(3) == 3
 
-    '''
-    return
+    """
+    def decorator(func):
+        def wrapper(*args):
+            print prefix, func.__name__ + '({})'.format(*args)
+        return wrapper
+    return decorator
 
 
-#@memo
+@memo
 @countcalls
 @n_ary
 def foo(a, b):
@@ -92,15 +106,15 @@ def foo(a, b):
 
 
 @countcalls
-#@memo
+@memo
 @n_ary
 def bar(a, b):
     return a * b
 
 
 @countcalls
-#@trace("####")
-#@memo
+@trace("####")
+@memo
 def fib(n):
     return 1 if n <= 1 else fib(n-1) + fib(n-2)
 
